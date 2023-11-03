a3820c0a0de1fb14fc57bff815fb31b575b5dcf8
dymafr <contact@dyma.fr>
2023-11-03 11:10:10 +0100

    Initial commit

---
+++ src/router.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+
+import { lazy } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const Admin = lazy(() => import('./pages/Admin/Admin'));
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'admin',
+        element: <Admin />,
+      },
+    ],
+  },
+]);
